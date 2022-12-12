779deaa126fa9066f3d81a04fc43ae159773179b
torytang <tory@torys-m-b-p.lan>
2022-12-12 10:43:32 +0800

    stlye: remove danmu

--- src/page/study-live/index.tsx
+++ src/page/study-live/index.tsx
@@ -1,18 +1,14 @@
 import { Bubble, ChatBubble, CountDown } from '@/components';
-import DanMuBubble from '@/components/danmu';
 import { LiveLayout } from '@/layout/live-layout';
-import { AnimatePresence, motion, Variants } from 'framer-motion';
 import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { timerAtom } from './model/atom';
 
-const danmuVariants: Variants = {
-  visible: { opacity: 1, x: 0 },
-  hidden: { opacity: 0, x: 200 },
-};
+// const danmuVariants: Variants = {
+//   visible: { opacity: 1, x: 0 },
+//   hidden: { opacity: 0, x: 200 },
+// };
 
 const StudyLive: React.FC = () => {
-  const timerState = useRecoilValue(timerAtom);
+  // const timerState = useRecoilValue(timerAtom);
 
   return (
     <LiveLayout>
@@ -29,7 +25,7 @@ const StudyLive: React.FC = () => {
           />
         }
       />
-      <AnimatePresence>
+      {/* <AnimatePresence>
         <motion.div
           variants={danmuVariants}
           animate={timerState.status === 'break' ? 'visible' : 'hidden'}
@@ -39,7 +35,7 @@ const StudyLive: React.FC = () => {
             className="absolute top-[4.5rem] right-[20px]"
           />
         </motion.div>
-      </AnimatePresence>
+      </AnimatePresence> */}
       <ChatBubble
         className="absolute bottom-[36px] right-[20px]"
         name="torytang"
