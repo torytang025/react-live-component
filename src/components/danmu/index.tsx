090faf3c76084431c37053020c9f375db7e6cdb5
torytang <tory@torys-m-b-p.local>
2023-03-22 22:28:24 +0800

    feat: use live ws npm

--- src/components/danmu/index.tsx
+++ src/components/danmu/index.tsx
@@ -1,10 +1,10 @@
+import { KeepLiveWS } from 'bilibili-live-ws/browser';
 import classNames from 'classnames';
 import dayjs, { Dayjs } from 'dayjs';
 import { AnimatePresence } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { Bubble } from '../chat-bubble';
-import { getWsEndpoint } from './utils';
 
 interface IMsg {
   name: React.ReactNode;
@@ -15,115 +15,88 @@ interface IMsg {
 }
 
 const maxMsg = 4;
-const rid = '26433151';
+const rid = 26433151;
 const TORY_ID = 21609301;
 
 export default function DanMuBubble(props: { className?: string }) {
   const { className } = props;
   const [msgList, setMsgList] = useState<IMsg[]>([]);
 
-  useEffect(() => {
-    const bliveEndpoint = getWsEndpoint();
-    if (!bliveEndpoint) return;
-    console.log(
-      '%c [ wss init success ]',
-      'font-size:13px; background:#FFFF00; color:#bf2c9f;',
-      bliveEndpoint,
-    );
-    const socket = new WebSocket(bliveEndpoint);
-
-    socket.addEventListener('open', () => {
-      // 进入房间命令
-      socket.send(
-        JSON.stringify({
-          cmd: 'enter', // 命令
-          rid, // 房间号
-          events: ['DANMU_MSG', 'SUPER_CHAT_MESSAGE',"SUPER_CHAT_MESSAGE_JPN"], // 监听这个房间中的事件列表
-        }),
+  const handleMessage = (info: any[]) => {
+    if (info) {
+      const data = info;
+      console.log(
+        '%c [ danmu data ]',
+        'font-size:13px; background:#FFFF00; color:#bf2c9f;',
+        data,
       );
-
-      // 离开房间命令
-      // socket.send(
-      //   JSON.stringify({
-      //     cmd: 'leave', // 命令
-      //     rid: '123', // 房间号
-      //   }),
-      // );
-    });
-
-    const handleMessage = (ev: MessageEvent) => {
-      const { data } = ev;
-      let jsonData;
-      try {
-        jsonData = JSON.parse(data);
-      } catch {
-        console.log();
+      if (!data || !data.length) {
+        return;
       }
-
-      if (jsonData && jsonData?.payload) {
-        const data = jsonData.payload?.info as any[];
-        console.log(
-          '%c [ danmu data ]',
-          'font-size:13px; background:#FFFF00; color:#bf2c9f;',
-          data,
+      const msg = data[1];
+      const user = data[2];
+      const userID = user[0];
+      const userName = user[1];
+      const fansInfo = data[3] as [number, string, string]; // 粉丝牌子等级 粉丝称呼
+      const key = uuidv4();
+      const msgData: IMsg = {
+        name: userName,
+        content: msg,
+        key,
+        time: dayjs(),
+      };
+      if (userID === TORY_ID) {
+        msgData.name = (
+          <div className="flex items-center gap-x-3">
+            <div className="flex items-center gap-x-1 rounded-2xl bg-front/20 p-2">
+              <span className="text-3xl">👾</span>
+              <span className="text-2xl font-extrabold text-primary-1">
+                999
+              </span>
+            </div>
+            <span>{userName}</span>
+          </div>
         );
-        if (!data || !data.length) {
-          return;
-        }
-        const msg = data[1];
-        const user = data[2];
-        const userID = user[0];
-        const userName = user[1];
-        const fansInfo = data[3] as [number, string, string]; // 粉丝牌子等级 粉丝称呼
-        const key = uuidv4();
-        const msgData: IMsg = {
-          name: userName,
-          content: msg,
-          key,
-          time: dayjs(),
-        };
-        if (userID === TORY_ID) {
-          msgData.name = (
-            <div className="flex items-center gap-x-3">
-              <div className="flex items-center gap-x-1 rounded-2xl bg-front/20 p-2">
-                <span className="text-3xl">👾</span>
-                <span className="text-2xl font-extrabold text-primary-1">
-                  999
-                </span>
-              </div>
-              <span>{userName}</span>
+      } else if (fansInfo.length && fansInfo[fansInfo.length - 1] === TORY_ID) {
+        msgData.name = (
+          <div className="flex items-center gap-x-3">
+            <div className="flex items-center gap-x-1 rounded-2xl bg-front/20 p-2">
+              <span className="text-3xl">🧙</span>
+              <span className="text-2xl font-extrabold text-primary-1">
+                {fansInfo[0]}
+              </span>
             </div>
-          );
-        } else if (
-          fansInfo.length &&
-          fansInfo[fansInfo.length - 1] === TORY_ID
-        ) {
-          msgData.name = (
-            <div className="flex items-center gap-x-3">
-              <div className="flex items-center gap-x-1 rounded-2xl bg-front/20 p-2">
-                <span className="text-3xl">🧙</span>
-                <span className="text-2xl font-extrabold text-primary-1">
-                  {fansInfo[0]}
-                </span>
-              </div>
-              <span>{userName}</span>
-            </div>
-          );
-        }
-        setMsgList((prev) => {
-          if (prev.length < maxMsg) {
-            return [...prev, msgData];
-          } else {
-            return [...prev.slice(1), msgData];
-          }
-        });
+            <span>{userName}</span>
+          </div>
+        );
       }
-    };
-
-    socket.addEventListener('message', handleMessage);
-    return () => {
-      socket.removeEventListener("message", handleMessage)
+      setMsgList((prev) => {
+        if (prev.length < maxMsg) {
+          return [...prev, msgData];
+        } else {
+          return [...prev.slice(1), msgData];
+        }
+      });
     }
+  };
+
+  useEffect(() => {
+    const live = new KeepLiveWS(rid);
+    live.on('open', () =>
+      console.log(
+        '%c [ wss init success ]',
+        'font-size:13px; background:#FFFF00; color:#bf2c9f;',
+      ),
+    );
+
+    live.on('msg', (data) => {
+      const type = data.cmd;
+      switch (type) {
+        case 'DANMU_MSG':
+          handleMessage(data.info);
+          break;
+      }
+    });
   }, []);
 
   // auto hide danmu
@@ -135,7 +108,7 @@ export default function DanMuBubble(props: { className?: string }) {
       setMsgList((prev) => {
         if (prev.length) {
           return prev.filter((item) => {
-            return !item.time.isBefore(dayjs().subtract(10, 'seconds'));
+            return !item.time.isBefore(dayjs().subtract(20, 'seconds'));
           });
         } else {
           return prev;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
     },
   },
   base: './',
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   build: {
     rollupOptions: {
       input: {
