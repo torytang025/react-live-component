52663e0ed1dcb5944de6eebcf04eef4bf5a0a1b6
torytang <torytang.025@gmail.com>
2023-04-09 17:32:53 +0800

    feat: danmu condition visible

--- src/page/study-live/index.tsx
+++ src/page/study-live/index.tsx
@@ -2,10 +2,20 @@ import { Bubble, ChatBubble, CountDown } from '@/components';
 import DanMuBubble from '@/components/danmu';
 import Headline from '@/components/headline';
 import { LiveLayout } from '@/layout/live-layout';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
+import { useRecoilValue } from 'recoil';
+import { timerAtom } from './model/atom';
+
+const danmuVariants: Variants = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: 200 },
+};
 
 const StudyLive: React.FC = () => {
+  const timerState = useRecoilValue(timerAtom);
+
   return (
     <>
       <LiveLayout>
@@ -22,10 +32,21 @@ const StudyLive: React.FC = () => {
             />
           }
         />
-        <DanMuBubble
-          key="danmu-bubble"
-          className="absolute top-[4.5rem] right-[20px] z-top"
-        />
+        <AnimatePresence>
+          {timerState.status === 'break' && (
+            <motion.div
+              variants={danmuVariants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+            >
+              <DanMuBubble
+                key="danmu-bubble"
+                className="absolute top-[4.5rem] right-[20px] z-top"
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
         <ChatBubble
           className="absolute bottom-[36px] right-[20px] z-top"
           name="torytang"
